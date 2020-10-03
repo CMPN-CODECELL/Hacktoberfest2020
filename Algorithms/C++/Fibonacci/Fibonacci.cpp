e0c07d1cf6159743fa2cab1d68f7fb7e3250f895
yashraj <yashrajdesai30@gmail.com>
2020-10-03 19:51:15 +0530

    Fibonacci Recursive solution

---
+++ Algorithms/C++/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int fib(int n) {
+
+    if(n==0) {
+        return 0;
+    }
+
+    if(n==1) {
+        return 1;
+    }
+
+    return fib(n-1)+fib(n-2);
+
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<fib(n);   //prints (n-1)th fibonacci term.
+}
+
+
