9bba643dfbc7df61856e569ca86f3839c1323805
Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>
2020-10-05 12:07:09 +0530

    Added print statements for better readability

--- Data Structures/Python/Queue/dqueue.py
+++ Data Structures/Python/Queue/dqueue.py
@@ -2,9 +2,12 @@
 
 from collections import deque
 
+print("Queues utilize the FIFO (First In First Out) Principle")
+print()
+
 q = deque() # Initializating Empty Queue
 
-print(q) # Printing empty queue
+print("Initial empty queue :", q) # Printing empty queue
 
 # Adding items to the queue
 q.append("Hey")
@@ -12,17 +15,17 @@ q.append("there!")
 q.append("This")
 q.append("is")
 q.append("a")
-q.append("Stack!")
+q.append("Queue!")
 
-print(q) # Printing queue with all elements
+print("After adding items into the queue:", q) # Printing queue with all elements
 
 q.popleft() # Removing an element from the queue
 
-print(q) # Printing queue after removing an element
+print("After removing an item from the queue:", q)# Printing queue after removing an element
 
 q.append("What?!") # Adding another element to the queue
 
-print(q) # Printing the new queue
+print("After adding an extra item to the queue:", q) # Printing the new queue
 
 # Removing all the elements from the queue
 q.popleft()
@@ -32,4 +35,6 @@ q.popleft()
 q.popleft()
 q.popleft()
 
-print(q) # Printing the empty queue
+print("After removing all the items:", q) # Printing the empty queue
+
+print("Trying to remove any more items will result in an error!")
