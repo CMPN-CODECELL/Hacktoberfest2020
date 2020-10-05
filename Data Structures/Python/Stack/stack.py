e67696268ac99dc029ad862fc7049273386d612c
Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>
2020-10-05 11:29:40 +0530

    Added print statements for better readability

--- Data Structures/Python/Stack/stack.py
+++ Data Structures/Python/Stack/stack.py
@@ -1,8 +1,11 @@
 # Python Program to demonstrate the LIFO principle using stack
 
+print("Stack utilizes the LIFO (Last In First Out) Principle")
+print()
+
 stack = [] # Initializating Empty Stack
 
-print(stack) # Printing empty Stack
+print("Initializing empty stack :", stack) # Printing empty Stack
 
 # Adding items to the Stack
 stack.append("Hey")
@@ -12,15 +15,15 @@ stack.append("is")
 stack.append("a")
 stack.append("Stack!")
 
-print(stack) # Printing stack after adding items
+print("After adding items into the stack:", stack) # Printing stack after adding items
 
 stack.pop() # Removing an item from the Stack
 
-print(stack) # Printing stack after removing an item
+print("After removing only one item from the stack:", stack) # Printing stack after removing an item
 
 stack.append("Retry!") # Re-adding an item to the Stack
 
-print(stack) # Printing the new Stack
+print("After re-adding an item to the stack:", stack) # Printing the new Stack
 
 # Removing all the items from the Stack
 stack.pop()
@@ -30,4 +33,6 @@ stack.pop()
 stack.pop()
 stack.pop()
 
-print(stack) # Printing empty Stack
+print("After removing all the items from the stack", stack) # Printing empty Stack
+
+print("Removing any more items will result in an error")
