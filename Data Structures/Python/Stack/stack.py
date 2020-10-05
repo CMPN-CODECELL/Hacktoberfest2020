744f068700339c3bc7b145c1bb593b123cc48453
Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>
2020-10-05 11:25:09 +0530

    Added comments

--- Data Structures/Python/Stack/stack.py
+++ Data Structures/Python/Stack/stack.py
@@ -1,6 +1,10 @@
+# Python Program to demonstrate the LIFO principle using stack
 
-stack = []
+stack = [] # Initializating Empty Stack
 
+print(stack) # Printing empty Stack
+
+# Adding items to the Stack
 stack.append("Hey")
 stack.append("there!")
 stack.append("This")
@@ -8,12 +12,22 @@ stack.append("is")
 stack.append("a")
 stack.append("Stack!")
 
-print(stack)
+print(stack) # Printing stack after adding items
 
+stack.pop() # Removing an item from the Stack
+
+print(stack) # Printing stack after removing an item
+
+stack.append("Retry!") # Re-adding an item to the Stack
+
+print(stack) # Printing the new Stack
+
+# Removing all the items from the Stack
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
 stack.pop()
 
-print(stack)
-
-stack.append("Retry!")
-
-print(stack)
+print(stack) # Printing empty Stack
