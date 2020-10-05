f9294e28dff892ec39800ba3f05354fb15a98339
Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>
2020-10-05 11:20:17 +0530

    Initial Stack Implementation

---
+++ Data Structures/Python/Stack/stack.py
@@ -0,0 +1,19 @@
+
+stack = []
+
+stack.append("Hey")
+stack.append("there!")
+stack.append("This")
+stack.append("is")
+stack.append("a")
+stack.append("Stack!")
+
+print(stack)
+
+stack.pop()
+
+print(stack)
+
+stack.append("Retry!")
+
+print(stack)
